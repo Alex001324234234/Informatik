d5a0ac184896cb126996fee06dfcf396ea39c2ab
Alex001324234234 <122977858+Alex001324234234@users.noreply.github.com>
2023-12-12 19:51:38 +0100

    weilichskann

--- main.py
+++ main.py
@@ -1,3 +1,836 @@
 def on_forever():
-    basic.show_string("hello World")
+    Wort = ["H","a","l","l","o"]
+    for i in range(len(Wort)):
+        if Wort[i] == "!":
+            z0()
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == '"':
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == '#':
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == '$':
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == '%':
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == '&':
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == '\'':
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == '(':
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == ')':
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == '+':
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == ',':
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == '.':
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == '/':
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+        elif Wort[i] == "0":
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "1":
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "2":
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "3":
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "4":
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "5":
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "6":
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "7":
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "8":
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "9":
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+        elif Wort[i] == ":":
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == ";":
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "<":
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "=":
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "?":
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "@":
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z0()
+            z0()
+        elif Wort[i] == "A":
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "B":
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "C":
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "D":
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "E":
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "F":
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "G":
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "H":
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "I":
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "J":
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "K":
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "L":
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+        elif Wort[i] == "M":
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "N":
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "O":
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "P":
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "Q":
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "R":
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "S":
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "T":
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "U":
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "V":
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "W":
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "X":
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "Y":
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "Z":
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "[":
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "\\":
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "]":
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "^":
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "_":
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "`":
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "a":
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "b":
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "c":
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "d":
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "e":
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "f":
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "g":
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "h":
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "i":
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "j":
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "k":
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "l":
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "m":
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "n":
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z0()
+            
+        elif Wort[i] == "o":
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "p":
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "q":
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "r":
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "s":
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "t":
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "u":
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "v":
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z0()
+
+        elif Wort[i] == "w":
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+            z1()
+
+        elif Wort[i] == "x":
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z0()
+
+        elif Wort[i] == "y":
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+            z1()
+
+        elif Wort[i] == "z":
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z0()
+
+        elif Wort[i] == "{":
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+            z1()
+
+        elif Wort[i] == "|":
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z0()
+
+        elif Wort[i] == "}":
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+            z1()
+
+        elif Wort[i] == "~":
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+            z1()
+            z0()
+
+
+
+
+def z0():
+    basic.pause(500)
+
+def z1():
+    basic.set_led_color(Colors.WHITE)
+    basic.pause(500)
+    basic.turn_rgb_led_off()
+    
+    
 basic.forever(on_forever)
+
+        
+    
+
+    
